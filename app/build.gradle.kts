a548f9da3aa5ea42f1d4d39b1b05e2ffef89f454
yuwenxi <veggetable@outlook.com>
2025-02-23 13:42:31 +0800

    Speed up. The search feature is updated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.text.newtextviewer"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
+        versionCode = 5
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
